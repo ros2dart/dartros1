31972e5e29db6e7a98233205c8c2593c2ae4f727
Tim Whiting <tim@whitings.org>
2020-05-19 09:23:52 -0600

    time

--- lib/src/time.dart
+++ lib/src/time.dart
@@ -1,8 +1,18 @@
-// TODO: Let user choose which time to use (ros sime or not)
 export 'utils/time_utils.dart';
+import 'utils/time_utils.dart';
+import '../dartros.dart';
 
 class Time {
+  static RosTime simTime = RosTime(secs: 0, nsecs: 0);
+  static bool useSimTime = false;
   static Future<void> initializeRosTime() async {
-    //TODO: Initialize ros time
+    try {
+      useSimTime = await nh.getParam('/use_sim_time');
+      if (useSimTime) {
+        // nh.subscribe('/clock', rosgraph_msgs.Clock, (msg) {}, throttleMs: -1);
+      }
+    } catch (e) {
+      rethrow;
+    }
   }
 }
